f2b97ebf9a24fe06112e60678211de939f07e559
sasaki <0704waffle@gmail.com>
2026-03-05 13:17:29 +0900

    a

--- src/julia/run_worker.jl
+++ src/julia/run_worker.jl
@@ -20,9 +20,9 @@ const STEPS = 1000000
 const SAVE_INT = 100
 const A = 0.5
 const dt = 0.01
-const cargo_radius = 5.0
+const cargo_radius = 2.0
 const start_seed = 1
-const end_seed = 201
+const end_seed = 101
 
 BASE_PATH = "D:\\Sasaki\\MTCargoSim\\MTC"
 
